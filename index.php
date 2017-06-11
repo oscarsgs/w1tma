9f299888d96283b12fbee7e8331560209e2e5928
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-11 21:50:25 +0100

    Added content to index.php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php 
+// Code to detect whether index.php has been requested without query string goes here
+// If no parameter detected...
+if (!isset($_GET['page'])) {
+    $id = '404'; // display home page
+} else {
+    $id = $_GET['page']; // else requested page
+}
+
+
+// Switch statement to decide content of page will go here.
+// Regardless of which "view" is displayed, the variable $content will
+switch ($id) {
+    case 'home' :
+        include 'views/home.php';
+        break;
+    case 'artists' :
+        include 'views/artists.php';
+        break;
+    case 'songs' :
+        include 'views/songs.php';
+        break;
+    default :
+        include 'views/404.php';
+}
+
 $header_template = "templates/header.html";
 $footer_template = "templates/footer.html";
 
