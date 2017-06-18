305392942e8cbf39605d8e20b0f876348cbedf67
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 18:53:04 +0100

    Added DB connection and queries on summary.php

--- includes/summary.php
+++ includes/summary.php
@@ -1,6 +1,6 @@
 <?php 
-	require 'includes/config.php';
-	require 'classes/db.php';
+	require_once 'includes/config.php';
+	require_once 'classes/db.php';
 	$aside_template = "templates/aside-summary.html";
 	$template = file_get_contents($aside_template);
 	$db = new DB($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
--- views/artists.php
+++ views/artists.php
@@ -1,6 +1,6 @@
 <?php 
-	require 'includes/config.php';
-	require 'classes/db.php';
+	require_once 'includes/config.php';
+	require_once 'classes/db.php';
 	$artist_head = "templates/content-header-artists.html";
 	$artist_content = "templates/content-artists.html";
 	$artist_foot = "templates/content-footer-artists.html";
