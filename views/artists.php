c45f598032f7768810a4a004313ac798169c0063
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 20:34:44 +0100

    Added functions.php on includes folder

--- views/artists.php
+++ views/artists.php
@@ -10,10 +10,12 @@
 	$artists = addArtists($config);
 	foreach ($artists as $artist) {
 		$artist->addSongsFromDB($config);
-		$template = file_get_contents($artist_content);
-		$artist_name = str_replace('%%-artist_name-%%', $artist->getName(), $template);
-		$artist_songs = str_replace('%%-artist_songs-%%', $artist->getSongCount(), $template);
-		$artist_table .= $artist_songs;
+		if($artist->getSongCount()>0){
+			$template = file_get_contents($artist_content);
+			$artist_name = str_replace('%%-artist_name-%%', $artist->getName(), $template);
+			$artist_songs = str_replace('%%-artist_songs-%%', $artist->getSongCount(), $template);
+			$artist_table .= $artist_songs;
+		}
 	}
 
 
