d9afa107bce09b2b853b3efef04c38cdf00beaf2
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 16:54:55 +0100

    Added template content to views

--- views/songs.php
+++ views/songs.php
@@ -1,7 +1,7 @@
 <?php 
 	$song_head = "templates/content-header-songs.html";
 	$song_content = "templates/content-songs.html";
-	$song_foot = "templates/content-footer.html";
+	$song_foot = "templates/content-footer-songs.html";
 	$content .= file_get_contents($song_head);
 	$content .= file_get_contents($song_content);
 	$content .= file_get_contents($song_foot);
