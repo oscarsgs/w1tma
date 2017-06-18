da83bc27844527b7377d6aa8a4bda92e170fe146
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 20:15:14 +0100

    Added functions.php on includes folder

--- views/artists.php
+++ views/artists.php
@@ -1,7 +1,7 @@
 <?php 
 	require_once 'includes/functions.php';
 	require_once 'includes/config.php';
-	require_once 'db.php';
+	require_once 'classes/db.php';
 	$artist_head = "templates/content-header-artists.html";
 	$artist_content = "templates/content-artists.html";
 	$artist_foot = "templates/content-footer-artists.html";
