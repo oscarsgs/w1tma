ad75fae7e646c1d9d50e16f5f62bd3b6b71b5e6a
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 18:02:37 +0100

    Added DB ojbect and minor changes to home.php

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 <?php 
 	$home_template = "templates/content-home.html";
 	$home_title = "Welcome to W1 Music!"
-	$welcome_message = "Pellentesque auctor convallis tellus a dictum. Fusce felis dolor, cursus a blandit."
+	$welcome_message = "Pellentesque auctor convallis tellus a dictum. Fusce felis dolor, cursus a blandit.";
 	$template = file_get_contents($home_template);
 	$title = str_replace('%%-home_title-%%', $home_title, $template);
 	$content .= str_replace('%%-welcome_message_content-%%', $welcome_message, $title);
