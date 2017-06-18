a96dca193326a1bdd87bf06ea9fe0e44901c98c0
Oscar Garcia-Saldarriaga <mail@oscarsgs.com>
2017-06-18 21:29:25 +0100

    W3C html5 validated

--- includes/functions.php
+++ includes/functions.php
@@ -17,4 +17,18 @@ function addArtists($c){
 		return $artists;
 }
 
+function addSummary($c){
+	$aside_template = "templates/aside-summary.html";
+	$template = file_get_contents($aside_template);
+	$db = new DB($c['db_host'], $c['db_user'], $c['db_pass'], $c['db_name']);
+	$sql = "SELECT * from song";
+	$query = $db->query($sql);
+    $songs = str_replace('%%-total_songs-%%', $query->num_rows, $template);
+	$sql = "SELECT distinct artist_id from artist a join song s on (a.id = s.artist_id)";
+	$query = $db->query($sql);
+	$result = str_replace('%%-total_artists-%%', $query->num_rows, $songs);
+	$db->close();
+	return $result;
+}
+
  ?>
--- includes/summary.php
+++ includes/summary.php
@@ -1,18 +1,5 @@
 <?php 
-	require_once 'includes/config.php';
-	require_once 'classes/db.php';
-	$aside_template = "templates/aside-summary.html";
-	$template = file_get_contents($aside_template);
-	$db = new DB($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
-	$sql = "SELECT * from song";
-	$query = $db->query($sql);
-    $songs = str_replace('%%-total_songs-%%', $query->num_rows, $template);
-	$sql = "SELECT distinct artist_id from artist a join song s on (a.id = s.artist_id)";
-	$query = $db->query($sql);
-    $summary .= str_replace('%%-total_artists-%%', $query->num_rows, $songs);
+	require_once 'includes/functions.php';
 
-	$db->close();
-	
-
-
+	$summary .= addSummary($config);
  ?>
